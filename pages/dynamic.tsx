2608dfda2685fc57dfe5cab8219da299f084b33e
ArleyOliveiraMendonca <arley@arley.com.br>
2022-05-19 16:22:13 -0300

    Renderização do lado do servidor

--- pages/dynamic.tsx
+++ pages/dynamic.tsx
@@ -1,13 +1,13 @@
-import { GetServerSideProps, NextPage } from "next"
-import { ReactNode, useEffect, useState } from "react"
-import { Col, Container, Row } from "reactstrap"
+import { GetServerSideProps, NextPage } from "next";
+import { ReactNode, useEffect, useState } from "react";
+import { Col, Container, Row } from "reactstrap";
 
-type ApiResponse = {
-  name: string
-  timestamp: Date
-}
+interface ApiResponse {
+    name: string
+    timestamp: Date
+  }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+  export const getServerSideProps: GetServerSideProps = async () => {
 	const serverSideData: ApiResponse = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res => res.json())
 
   return {
@@ -17,42 +17,48 @@ export const getServerSideProps: GetServerSideProps = async () => {
   }
 }
 
-const Dynamic: NextPage = (props: {
-    children?: ReactNode
-    serverSideData?: ApiResponse
-  }) => {
-  const [clientSideData, setClientSideData] = useState<ApiResponse>()
+function Dynamic(props: {
+    children?: ReactNode;
+    serverSideData?: ApiResponse;
+}) {
+    const [clientSideData, setClientSideData] = useState<ApiResponse>();
 
-  useEffect(() => {
-    fetchData()
-  }, [])
 
-  const fetchData = async () => {
-    const data = await fetch("/api/hello").then(res => res.json())
-    setClientSideData(data)
-  }
+    const Dynamic: NextPage = () => {
+        const [clientSideData, setClientSideData] = useState<ApiResponse>();
 
-  return (
-    <Container tag="main">
-      <h1 className="my-5">
-        Como funcionam as renderizações do Next.js
-      </h1>
+        useEffect(() => {
+            fetchData();
+        }, []);
 
-      <Row>
-        <Col>
-          <h3>
-            Gerado no servidor: {props.serverSideData?.timestamp}
-          </h3>
-        </Col>
+        const fetchData = async () => {
+            const data = await fetch("/api/hello").then(res => res.json());
+            setClientSideData(data);
+        };
 
-        <Col>
-          <h3>
-            Gerado no cliente: {clientSideData?.timestamp}
-          </h3>
-        </Col>
-      </Row>
-    </Container>
-  )
+        return (
+            <Container tag="main">
+                <h1 className="my-5">
+                    Como funcionam as renderizações do Next.js
+                </h1>
+
+                <Row>
+                    <Col>
+                        <h3>
+                            Gerado no servidor:
+                        </h3>
+                    </Col>
+
+                    <Col>
+                        <h3>
+                            Gerado no cliente:
+                        </h3>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    };
+
+   
 }
-
-export default Dynamic
+ export default Dynamic;
